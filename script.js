e66c789c0d27dd4dfc7cbbc8c0c9ef09ccec488a
Ytshplays <79090801+Ytshplays@users.noreply.github.com>
2022-12-11 05:24:12 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt('What is your first Name?');
+var lastname = prompt('What is your Last Name?'); alert ('Hi'+' '+name+' '+lastname)
+var age = prompt ('What is your age');
+
+if (age<13) {
+alert ("soz bro you can't enter")
+} 
+if(age>13) {
+
+    alert ("You are allowed in lets goo!!!")
+    }
+    if(age==13){
+        alert("You can come in FINALLY!!!!")
+    }
+    
